e60e26d381b41246af0863d8f0ffb74824e46838
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-04 12:17:31 +0900

    resourceの作成、基礎の確認だね♪

---
+++ src/items/items.controller.ts
@@ -0,0 +1,12 @@
+import { ItemsService } from './items.service';
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll() {
+    return this.itemsService.findAll();
+  }
+}
