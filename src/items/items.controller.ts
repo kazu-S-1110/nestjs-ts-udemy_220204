e5dd56021f2584468b7b9c6055c28abf11fe5b21
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-05 21:51:28 +0900

    dtoの役割がやっとわかったわ！！！！

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,3 @@
-import { ItemStatus } from './item-status.enum';
 import { ItemsService } from './items.service';
 import {
   Body,
@@ -10,6 +9,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { Item } from './item.model';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -27,19 +27,13 @@ export class ItemsController {
 
   @Post()
   create(
-    @Body('id') id: string,
-    @Body('name') name: string,
-    @Body('price') price: number,
-    @Body('description') description: string,
+    // @Body('id') id: string,
+    // @Body('name') name: string,
+    // @Body('price') price: number,
+    // @Body('description') description: string,
+    @Body() createItemDto: CreateItemDto,
   ): Item {
-    const item: Item = {
-      id,
-      name,
-      price,
-      description,
-      status: ItemStatus.ON_SALE,
-    };
-    return this.itemsService.create(item);
+    return this.itemsService.create(createItemDto);
   }
 
   @Patch(':id')
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,3 +1,4 @@
+import { CreateItemDto } from './dto/create-item.dto';
 import { Injectable } from '@nestjs/common';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
@@ -14,7 +15,11 @@ export class ItemsService {
     return this.items.find((item) => item.id === id);
   }
 
-  create(item: Item): Item {
+  create(createItemDto: CreateItemDto): Item {
+    const item: Item = {
+      ...createItemDto,
+      status: ItemStatus.ON_SALE,
+    };
     this.items.push(item);
     return item;
   }
