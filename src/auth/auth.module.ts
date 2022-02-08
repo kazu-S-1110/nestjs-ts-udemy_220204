048e2e4b11e3471688c1aee18cf93352d1fd2e05
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-09 08:01:36 +0900

    jwtModuleを設定。

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }), //認証形式を選択する
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: {
+        expiresIn: 3600, //jwtの認証期限、単位はseconds
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
