ffcf65a75b5d73a15f8b356e885e684b25002af9
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-09 08:22:10 +0900

    dtoとserviceを実装。jwtServiceの挙動を調べておく。

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,36 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
     return await this.userRepository.createUser(createUserDto);
+  }
+
+  async signIn(
+    credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = credentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    // userが存在して且つbcrypt.compareで正しいかどうか検証する
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { id: user.id, username: user.username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
+    }
+
+    throw new UnauthorizedException('Username or Password is invalid, lol');
   }
 }
